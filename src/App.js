672fa1594253c7aebe773dfd3bce5b5c1cfc8450
Rohan <rohanmv22@gmail.com>
2024-10-21 16:01:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from "./components/Header"
+import Home from './components/Home';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Routes>
+          <Route path = '/' exact element={<Home />}/>
+          <Route path = '/cart' exact element={<Cart />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
